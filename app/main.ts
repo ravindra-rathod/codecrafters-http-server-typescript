2d8d3774f734b41eacc862ebfd38dcebe5b13b25
ravindra-rathod <rathodravindra4494@gmail.com>
2025-04-09 00:38:53 +0530

    added 200 response

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,22 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on("close", () => {
-        socket.end();
-    });
+    console.log("Request came");
+    var res: string = buildResponse(200, "OK");
+    socket.write(res);
+    socket.end();
 });
+
+function buildResponse(status: number, reason: string): string {
+    const CRLF: string = "\r\n"
+    const statusLine = `HTTP/1.1 ${status} ${reason}${CRLF}`;
+    const headers = `${CRLF}`
+    const body = "";
+    var response = statusLine + headers + body;
+    console.log("Returning Reponse")
+    console.log(JSON.stringify({ response }))
+    return response;
+}
 
 server.listen(4221, "localhost");
