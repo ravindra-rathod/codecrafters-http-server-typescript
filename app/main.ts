fc3be4b234629f6a9492dae724e502faae5a1fba
ravindra-rathod <rathodravindra4494@gmail.com>
2025-04-09 01:09:53 +0530

    Added a reques parser

--- app/main.ts
+++ app/main.ts
@@ -4,14 +4,53 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-    console.log("Request came");
-    var res: string = buildResponse(200, "OK");
-    socket.write(res);
-    socket.end();
+    socket.on("data", (reqData) => {
+
+        var req = parseRequest(reqData.toString())
+        console.log("Request came", req);
+
+        var res: string
+        if (req.path == "/" && req.httpMethod == "GET") {
+            res = buildResponse(200, "OK");
+        } else {
+            res = buildResponse(404, "Not Found")
+        }
+
+        socket.write(res);
+        socket.end();
+    })
+
+
 });
 
+
+interface HttpRequest {
+    httpMethod: string,
+    path: string,
+    headers: Map<String, String>,
+    body: any
+
+}
+const CRLF: string = "\r\n"
+function parseRequest(req: string): HttpRequest {
+    var reqs = req.split(CRLF)
+    var requestLine = reqs[0].split(" ")
+    var headers: Map<String, String> = new Map();
+    for (var i = 1; i < reqs.length - 2; i++) {
+        var headerValue = reqs[i].split(":");
+        headers.set(headerValue[0], headerValue[1])
+    }
+    var body = reqs[reqs.length - 1]
+    return {
+        httpMethod: requestLine[0],
+        path: requestLine[1],
+        headers,
+        body
+    }
+
+}
 function buildResponse(status: number, reason: string): string {
-    const CRLF: string = "\r\n"
+
     const statusLine = `HTTP/1.1 ${status} ${reason}${CRLF}`;
     const headers = `${CRLF}`
     const body = "";
